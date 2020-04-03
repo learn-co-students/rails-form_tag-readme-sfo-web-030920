ae448808233f824ffe20e16cc40c5e5b6d029cde
caicindy87 <caicindy87@gmail.com>
2020-04-02 21:35:39 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Post.create(title: "Avocado", description: "Healthy fruit")
+Post.create(title: "Dark Chocolate", description: "Has antioxidants")
